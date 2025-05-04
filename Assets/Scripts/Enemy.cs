56db4357c9779934ea54f797a8311df464a1c4d3
DidacCordero <114507225+vjp-DidacCorderoSuarez@users.noreply.github.com>
2025-05-04 21:07:12 +0200

    Spawneo de Enemigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Enemy : Node
+public partial class Enemy : Area2D
 {
 		// En el enemigo (puede estar en un script del enemigo o del Main)
 	private void _on_body_entered(Node body)
